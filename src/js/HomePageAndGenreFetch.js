0685d3350f776233bf10e2b5c7099836c4ec8699
Andrey1914 <anlikajw@gmail.com>
2022-07-15 13:04:28 +0200

    Update HomePageAndGenreFetch.js

--- src/js/HomePageAndGenreFetch.js
+++ src/js/HomePageAndGenreFetch.js
@@ -1,4 +1,6 @@
+import { makeMarkupCard } from './cardMarkup';
 import { Movie } from './fetchMovie';
+import { makeMarkupCard } from './cardMarkup';
 
 const LS_GENRE_KEY = 'themoviedb.org-genre';
 
@@ -11,17 +13,7 @@ async function startPageVisit() {
     .fetchTrend()
     .then(data => {
       console.log(data);
-      data.results.map(movieItem => {
-        // console.log(movieItem);
-        console.log(`Poster: ${Movie.IMG_PATH + movieItem.poster_path}`);
-        console.log(`Title: ${movieItem.title}`);
-        console.log(
-          `${genreFind(movieItem.genre_ids)} | ${parseInt(
-            movieItem.release_date,
-            10
-          )}`
-        );
-      });
+      makeMarkupCard(data);
     })
     .catch(error => console.log(error));
 }
@@ -38,7 +30,7 @@ async function genreLoad(classInstance) {
   }
 }
 
-function genreFind(genreList = []) {
+export function genreFind(genreList = []) {
   const genreLS = localStorage.getItem(LS_GENRE_KEY);
   const noGenre = 'No genres';
   const genreOther = 'Other';
