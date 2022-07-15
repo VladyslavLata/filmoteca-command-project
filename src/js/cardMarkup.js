16e037e25702c164ea4056f224098763131b0849
Andrey1914 <anlikajw@gmail.com>
2022-07-15 13:04:17 +0200

    Create cardMarkup.js

---
+++ src/js/cardMarkup.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { Movie } from './fetchMovie';
+import { genreFind } from './HomePageAndGenreFetch';
+
+export function makeMarkupCard(data) {
+  const gallery = document.querySelector('.gallery');
+  const makeMarkupCard = data.results
+    .map(movieItem => {
+      return `<li class="card">
+          <a class="card__link" href="">
+            <img class="card__img" src="${
+              // ?
+              Movie.IMG_PATH + movieItem.poster_path
+              // : 'https://upload.wikimedia.org/wikipedia/commons/b/ba/No_image_available_400_x_600.svg'
+            }" alt="${movieItem.title}" />
+            <p class="card__name">${movieItem.title}</p>
+            <p class="card__description">${genreFind(
+              movieItem.genre_ids
+            )} | ${parseInt(movieItem.release_date, 10)}
+            </p>
+            <span class="card__vote">${movieItem.vote_average}</span>
+          </a>
+        </li>`;
+    })
+    .join('');
+  gallery.innerHTML = makeMarkupCard;
+}
