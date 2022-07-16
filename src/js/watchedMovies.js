d749e12b014cc40c1fe7466e0622fc3f29125bac
VladyslavLata <vladlatahtc@gmail.com>
2022-07-16 18:39:07 +0300

    Update watchedMovies.js

--- src/js/watchedMovies.js
+++ src/js/watchedMovies.js
@@ -1,40 +1,47 @@
+import { makeMarkupCard } from './cardMarkup';
 
 const watchedMovieBtnEl = document.querySelector('.watched');
 const galleryEl = document.querySelector('.gallery');
 let watchedFilms = null;
 let watchedFilmsLength = 0;
+let page = 1;
 
 watchedMovieBtnEl.addEventListener('click', onClickWatchedBtnMarkupFilms);
 
 function onClickWatchedBtnMarkupFilms() {
   galleryEl.innerHTML = '';
   watchedFilms = getWatchedFilmsLocalStorage();
+  console.log('2', watchedFilms);
   if (watchedFilms === null) {
-    return;
-  } else if (watchedFilms === undefined) {
     noFilmsMessage();
     return;
-  } else {
+  } else if (watchedFilms === undefined) {
+    return;
+  } else if (watchedFilms){
     watchedFilmsLength = watchedFilms.length;
     console.log(watchedFilmsLength);
+    console.log(watchedFilms);
+    makeMarkupCard(watchedFilms);
   }
 }
 
-
-function getWatchedFilmsLocalStorage() { 
+function getWatchedFilmsLocalStorage() {
   try {
-    const getWatchedFilms = localStorage.getItem("FILMS");
-    return getWatchedFilms === null ? undefined : JSON.parse(getWatchedFilms);
+    const getWatchedFilms = localStorage.getItem('FILMS');
+    console.log('1:', getWatchedFilms);
+    return getWatchedFilms === null ? null : JSON.parse(getWatchedFilms);
   } catch (error) {
-    console.error("Get state error: ", error.message);
     errorMessage();
+    console.error('Get state error: ', error.message);
   }
 }
 
 function noFilmsMessage() {
-  boxEl.innerHTML = '<p class="message">Your watch films list is empty.</p>';
+  galleryEl.innerHTML =
+    '<p class="message">Your watch films list is empty.</p>';
 }
 
 function errorMessage() {
-   boxEl.innerHTML = '<b class="error">Unknown error. Watched movies cannot be displayed.</b>';
+  galleryEl.innerHTML =
+    '<p class="error">Unknown error. Watched movies cannot be displayed.</p>';
 }
