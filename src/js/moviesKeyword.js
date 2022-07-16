0037af18cf0547049b97e0fdcfc7879c99ea7a1b
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-07-16 16:14:24 +0300

    Update moviesKeyword.js

--- src/js/moviesKeyword.js
+++ src/js/moviesKeyword.js
@@ -9,28 +9,28 @@ const refs = {
 refs.formEl.addEventListener('submit', onClickSubmit);
 
 async function onClickSubmit(event) {
-    try {
-      event.preventDefault();
-      const keyword = event.target.query.value.trim();
+  try {
+    event.preventDefault();
+    const keyword = event.target.query.value.trim();
 
-      if(keyword === '') {
-        refs.paragraphEl.innerHTML = `Enter the name in the search field`;
-        return false;
-      };
-
-      const keywordMovies = new Movie(keyword);
-      const data = await keywordMovies.fetchSearch();
-
-      if(data.total_results === 0) {
-        refs.paragraphEl.innerHTML = `Search result not successful. Enter the correct movie name and`;
-        return false;
-      };
-
-        makeMarkupCard(data);
-        event.target.reset();
-        refs.paragraphEl.innerHTML = "";
-
-    } catch (error) {
-        console.log(error.message);
+    if (keyword === '') {
+      refs.paragraphEl.innerHTML = `Enter the name in the search field`;
+      return;
     }
-};
+
+    const keywordMovies = new Movie(keyword);
+    const data = await keywordMovies.fetchSearch();
+
+    if (data.total_results === 0) {
+      event.target.reset();
+      refs.paragraphEl.innerHTML = `Search result not successful. Enter the correct movie name and try again.`;
+      return;
+    }
+
+    makeMarkupCard(data);
+    event.target.reset();
+    refs.paragraphEl.innerHTML = '';
+  } catch (error) {
+    console.log(error.message);
+  }
+}
