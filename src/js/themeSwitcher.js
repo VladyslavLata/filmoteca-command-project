d252bab2fc1e6d630c0a7427d157aab0536cdbc6
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-07-18 11:38:15 +0300

    Added additional functional

--- src/js/themeSwitcher.js
+++ src/js/themeSwitcher.js
@@ -1,15 +1,14 @@
 const body = document.querySelector('body');
-const iconLink = document.querySelector('.icon__button');
+const iconButton = document.querySelector('.icon__button');
 const iconSun = document.querySelector('.icon__sun');
 const iconMoon = document.querySelector('.icon__moon');
-// let darkMode = localStorage.getItem('dark-mode');
 export const themeKey = {
   LS_KEY: 'theme-mode',
   LIGHT: 'light',
   DARK: 'dark',
 };
 
-iconLink.addEventListener('click', toggleTheme);
+iconButton.addEventListener('click', toggleTheme);
 
 const enableDarkMode = () => {
   body.classList.add('dark__theme');
@@ -53,30 +52,3 @@ function startTheme() {
       break;
   }
 }
-
-// const enableDarkMode = () => {
-//   body.classList.add('dark__theme');
-//   iconMoon.classList.add('icon__item--hidden');
-//   iconSun.classList.remove('icon__item--hidden');
-//   localStorage.setItem('dark-mode', 'enabled');
-// };
-
-// const disableDarkMode = () => {
-//   body.classList.remove('dark__theme');
-//   iconMoon.classList.remove('icon__item--hidden');
-//   iconSun.classList.add('icon__item--hidden');
-//   localStorage.setItem('dark-mode', 'disabled');
-// };
-
-// if (darkMode === 'enabled') {
-//   enableDarkMode();
-// }
-
-// function toggleTheme(e) {
-//   darkMode = localStorage.getItem('dark-mode');
-//   if (darkMode === 'disabled') {
-//     enableDarkMode();
-//   } else {
-//     disableDarkMode();
-//   }
-// }
