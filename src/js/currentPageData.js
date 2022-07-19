ac7af3d778556aa7040ca82d6c678cb35e713e6e
VladyslavLata <vladlatahtc@gmail.com>
2022-07-19 15:12:48 +0300

    j

--- src/js/currentPageData.js
+++
@@ -1,9 +0,0 @@
-const LS_CURRENT_DATA_KEY = 'themoviedb-current-data';
-
-export function setCurrenDataToLS(data) {
-  localStorage.setItem(LS_CURRENT_DATA_KEY, JSON.stringify(data));
-}
-
-export function getCurrenDataFromLS(data) {
-  return JSON.parse(localStorage.getItem(LS_CURRENT_DATA_KEY));
-}
