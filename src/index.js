0a66c03954a9677f859cec5a085d43692f6c83dc
SvitlanaVelychko <svel2608@gmail.com>
2022-07-19 19:51:30 +0300

    Change btn switcher styles in header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as movie from './js/homePage';
 import * as language from './js/languageSwitch';
-import * as language from './js/trendTime';
+import * as trendTime from './js/trendTime';
 import * as pagination from './js/paginationPage';
 import * as theme from './js/themeSwitcher';
 import * as moviesKeyword from './js/moviesKeyword';
