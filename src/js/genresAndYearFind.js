bb9ee500b1f2ce6eb9a0c5f02ae4727f4fd32f36
VladyslavLata <vladlatahtc@gmail.com>
2022-07-19 14:11:26 +0300

    е

---
+++ src/js/genresAndYearFind.js
@@ -0,0 +1,66 @@
+import { Movie } from './fetchMovie';
+
+export const keyLS = {
+  LS_LANGUAGE_KEY: 'themoviedb-current-language',
+  LS_GENRE_KEY_EN: 'themoviedb-genre-EN',
+  LS_GENRE_KEY_UA: 'themoviedb-genre-UA',
+};
+
+//  genre--------------->
+
+export function genreFind(genreList = []) {
+  let genreLS = localStorage.getItem(keyLS.LS_GENRE_KEY_EN);
+  let noGenre = 'No genres';
+  let genreOther = 'Other';
+  const langGenre = getLanguageFromLS();
+
+  switch (langGenre) {
+    case Movie.language.ENGLISH:
+      genreLS = localStorage.getItem(keyLS.LS_GENRE_KEY_EN);
+      noGenre = 'No genres';
+      genreOther = 'Other';
+      break;
+
+    case Movie.language.UKRAINIAN:
+      genreLS = localStorage.getItem(keyLS.LS_GENRE_KEY_UA);
+      noGenre = 'Жанри відсутні';
+      genreOther = 'Інші';
+      break;
+  }
+
+  if (!genreLS || genreList.length === 0) {
+    return noGenre;
+  }
+  const genreArray = JSON.parse(genreLS);
+  const genreResult = genreArray.reduce((previousValue, element) => {
+    if (genreList.includes(element.id)) {
+      previousValue.push(element.name);
+    }
+    return previousValue;
+  }, []);
+
+  if (genreResult.length === 0) {
+    return noGenre;
+  } else if (genreResult.length > 3) {
+    return `${genreResult[0]}, ${genreResult[1]}, ${genreOther}`;
+  } else {
+    return genreResult.join(', ');
+  }
+}
+// <-----------------genre
+
+function getLanguageFromLS() {
+  return JSON.parse(localStorage.getItem(keyLS.LS_LANGUAGE_KEY));
+}
+
+export function noYearVariableLang(yearValue) {
+  const currentLang = getLanguageFromLS();
+  switch (currentLang) {
+    case Movie.language.ENGLISH:
+      return !yearValue ? 'No year' : parseInt(yearValue, 10);
+
+    case Movie.language.UKRAINIAN:
+      return !yearValue ? 'Немає року' : parseInt(yearValue, 10);
+  }
+  return parseInt(yearValue, 10);
+}
--- src/js/watchedMovies.js
+++ src/js/watchedMovies.js
@@ -1,15 +1,12 @@
 import { Movie } from './fetchMovie';
+import { genreFind, noYearVariableLang } from './genresAndYearFind'
 // import { genreFind } from './workWithGenres';
 // import {keyLS } from './languageSwitch';
 // import {  getLanguageFromLS  } from './languageSwitch';
 // import { genreFind } from './workWithGenres';
 // import { noYearVariableLang } from './languageSwitch';
 
-const keyLS = {
-  LS_LANGUAGE_KEY: 'themoviedb-current-language',
-  LS_GENRE_KEY_EN: 'themoviedb-genre-EN',
-  LS_GENRE_KEY_UA: 'themoviedb-genre-UA',
-};
+
 
 // const refss = {
 //   btnSwitchEN: document.querySelector('.switch-EN'),
@@ -348,61 +345,4 @@ function handledChangeDeskTop(e) {
   }
 }
 
-//  genre--------------->
 
-function genreFind(genreList = []) {
-  let genreLS = localStorage.getItem(keyLS.LS_GENRE_KEY_EN);
-  let noGenre = 'No genres';
-  let genreOther = 'Other';
-  const langGenre = getLanguageFromLS();
-
-  switch (langGenre) {
-    case Movie.language.ENGLISH:
-      genreLS = localStorage.getItem(keyLS.LS_GENRE_KEY_EN);
-      noGenre = 'No genres';
-      genreOther = 'Other';
-      break;
-
-    case Movie.language.UKRAINIAN:
-      genreLS = localStorage.getItem(keyLS.LS_GENRE_KEY_UA);
-      noGenre = 'Жанри відсутні';
-      genreOther = 'Інші';
-      break;
-  }
-
-  if (!genreLS || genreList.length === 0) {
-    return noGenre;
-  }
-  const genreArray = JSON.parse(genreLS);
-  const genreResult = genreArray.reduce((previousValue, element) => {
-    if (genreList.includes(element.id)) {
-      previousValue.push(element.name);
-    }
-    return previousValue;
-  }, []);
-
-  if (genreResult.length === 0) {
-    return noGenre;
-  } else if (genreResult.length > 3) {
-    return `${genreResult[0]}, ${genreResult[1]}, ${genreOther}`;
-  } else {
-    return genreResult.join(', ');
-  }
-}
-
-function getLanguageFromLS() {
-  return JSON.parse(localStorage.getItem(keyLS.LS_LANGUAGE_KEY));
-}
-// <-----------------genre
-
-function noYearVariableLang(yearValue) {
-  const currentLang = getLanguageFromLS();
-  switch (currentLang) {
-    case Movie.language.ENGLISH:
-      return !yearValue ? 'No year' : parseInt(yearValue, 10);
-
-    case Movie.language.UKRAINIAN:
-      return !yearValue ? 'Немає року' : parseInt(yearValue, 10);
-  }
-  return parseInt(yearValue, 10);
-}
