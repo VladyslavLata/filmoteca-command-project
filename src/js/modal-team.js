d489bbd4609df4da6a89f69a2266f4435cf5697b
AleksandrTikka <alexandertikka@gmail.com>
2022-07-20 00:54:55 +0300

    modal_team
    не хватает фото, жду пожеланий по оформлению

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,6 +1,7 @@
 const team = document.querySelector('.footer-text__link');
 const backdropTeam = document.querySelector('.backdrop-team');
 const closeTeam = document.querySelector('.modal-team__button');
+const body = document.querySelector('body');
 
 team.addEventListener('click', onTeamClick);
 closeTeam.addEventListener('click', onCloseTeamClick);
@@ -10,10 +11,12 @@ closeTeam.addEventListener('click', onCloseTeamClick);
 function onTeamClick(e) {
     e.preventDefault();
     backdropTeam.classList.remove('is-hidden');
+    body.classList.add('modal-team-open');
 
 };
 
 function onCloseTeamClick(e) {
     e.preventDefault();
     backdropTeam.classList.add('is-hidden');
+    body.classList.remove('modal-team-open');
     };
