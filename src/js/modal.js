84504cbaa6d3e336d5b42a840f2b83ffcb3a2c45
SvitlanaVelychko <svel2608@gmail.com>
2022-07-23 00:55:52 +0300

    Change style modal btn

--- src/js/modal.js
+++ src/js/modal.js
@@ -204,7 +204,6 @@ async function onBtnClick(evt) {
     if ((username !== '' && username) || (usernameSS !== '' && usernameSS)) {
       // addToWatched(evt);
       addToWatched(evt);
-      evt.target.setAttribute('data-action', 'del');
       writeTestCollectionFunction();
     } else {
       alert(
@@ -215,7 +214,6 @@ async function onBtnClick(evt) {
   if (evt.target.name === btnNameKey.QUEUE) {
     if ((username !== '' && username) || (usernameSS !== '' && usernameSS)) {
       addToQueue(evt);
-      evt.target.setAttribute('data-action', 'del');
       writeTestCollectionFunction();
     } else {
       alert(
