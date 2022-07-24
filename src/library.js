52bd945f37f76ca8eac09e21bc6abc8ab5c5b567
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-07-24 21:52:09 +0300

    Fixed library up and switcher

--- src/library.js
+++ src/library.js
@@ -1,10 +1,10 @@
-
 import * as loader from './js/loader';
 import * as watchedMovies from './js/watchedMovies';
 // import * as pg from './js/pg';
 import * as theme from './js/themeSwitcher';
 import * as languageLibrary from './js/languageLibrary';
 import * as modal from './js/modal';
+import * as up from './js/up-btnAndSwitcher';
 import * as footerTranslation from './js/footerTranslation';
 import * as modalTeam from './js/modal-team';
 import * as signIn from './js/switchSignInForms';
